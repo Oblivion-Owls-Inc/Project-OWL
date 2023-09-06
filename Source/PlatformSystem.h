8a3c929fef5e5ee7b16da42f23ccb75bee6eb11c
teliVision <teliVision@users.noreply.github.com>
2023-09-05 21:32:47 -0700

    Got the Platform System to work.

--- Source/PlatformSystem.h
+++ Source/PlatformSystem.h
@@ -1,14 +1,28 @@
 #pragma once
+#include "System.h"
+#include "glm/vec2.hpp"
 
-#include "BaseSystem.h"
-
-class GLFWwindow;
+struct GLFWwindow;	// forward reference for GetWindowHandle()
 
 class PlatformSystem : public System
 {
 public:
-	PlatformSystem(const char* name, int w_width, int w_height);
-	GLFWwindow* GetWindowHandle();
+	PlatformSystem(const char* w_name = "Prototype", int w_width = 800, int w_height = 600);
+	GLFWwindow* GetWindowHandle() const;
+	bool WindowClosing() const;
+
+	// Returns width and height as a vec2
+	glm::vec2 GetWindowDimensions() const;
+
+	// Prevent copying (later, gotta prevent multiple instances in general)
+	PlatformSystem(PlatformSystem& other) = delete;
+	void operator=(const PlatformSystem&) = delete;
+
+
+private:
+	int _width, _height;
+	GLFWwindow* _window;
+	bool _initialized;
 
 	// Inherited via System
 	virtual void OnInit() override;
@@ -20,8 +34,4 @@ public:
 	virtual void OnSceneLoad() override {}
 	virtual void OnSceneInit() override {}
 	virtual void OnSceneExit() override {}
-
-private:
-	int _width, _height;
-	GLFWwindow* _window;
 };
