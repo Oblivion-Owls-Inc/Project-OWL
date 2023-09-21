2f415a251e91be9f5ad6da71b19fe1bc279f3340
SixSleepyAsh <Claytonjax88@gmail.com>
2023-09-20 18:19:11 -0700

    updated main

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include "Engine.h"
-#include "PlatformSystem.h"
 #include "DebugSystem.h"
 #include "SceneSystem.h"
 #include "RenderSystem.h"
@@ -9,24 +8,46 @@
 #pragma warning(disable : 33019) //Disabling warning for missing PDB files
 
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+int main ()
 {
-    UNREFERENCED_PARAMETER(hInstance);
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(pCmdLine);
-    UNREFERENCED_PARAMETER(nCmdShow);
+	// *** Initialize the window using glfw *******************************************************
 
-    // *** Initialize the window using glfw *******************************************************
+	Engine* engine = Engine::getInstance();
 
-    Engine* engine = Engine::getInstance();
+	// engine->AddSystem( PlatformSystem::getInstance() );
+	// engine->AddSystem( SceneSystem::getInstance( "SceneName" ) );
+	// engine->AddSystem( RenderSystem::getInstance() );
+	// engine->AddSystem( DebugSystem::getInstance() );
 
-    // engine->AddSystem( PlatformSystem::getInstance() );
-    // engine->AddSystem( SceneSystem::getInstance( "SceneName" ) );
-    // engine->AddSystem( RenderSystem::getInstance() );
-    // engine->AddSystem( DebugSystem::getInstance() );
+	engine->Run();
 
-    engine->Run();
-
-    return 0;
+	return 0;
 
 }
+
+//
+//
+//int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+//{
+//    UNREFERENCED_PARAMETER(hInstance);
+//    UNREFERENCED_PARAMETER(hPrevInstance);
+//    UNREFERENCED_PARAMETER(pCmdLine);
+//    UNREFERENCED_PARAMETER(nCmdShow);
+//
+//    // *** Initialize the window using glfw *******************************************************
+//
+//    Engine* engine = Engine::getInstance();
+//
+//    // engine->AddSystem( PlatformSystem::getInstance() );
+//    // engine->AddSystem( SceneSystem::getInstance( "SceneName" ) );
+//    // engine->AddSystem( RenderSystem::getInstance() );
+//    // engine->AddSystem( DebugSystem::getInstance() );
+//
+//    engine->Run();
+//
+//    return 0;
+//
+//}
+
+
+
