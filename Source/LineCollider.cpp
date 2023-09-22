58e38f30ac210592d65decb07500f088fffb5582
aidanstraker <aidanstraker@users.noreply.github.com>
2023-09-22 16:13:42 -0700

    Merging LineCollider

---
+++ Source/LineCollider.cpp
@@ -0,0 +1,89 @@
+#include "glm/glm.hpp"
+#include "Entity.h"
+#include "Sprite.h"
+#include "RigidBody.h"
+#include "Transform.h"
+
+#include "LineCollider.h"
+#include "CircleCollider.h"
+
+LineCollider::LineCollider(): Collider()
+{
+	setColliderType(ColliderType::ColliderTypeLine);
+}
+
+Component* LineCollider::Clone() const
+{
+	return nullptr;
+}
+
+void LineCollider::AddLineSegment(const vec2* p0, const vec2* p1)
+{
+	ColliderLineSegment temp{};
+	temp.point[0] = *p0;
+	temp.point[1] = *p1;
+	m_lineSegments.push_back(temp);
+	lineCount++;
+}
+
+void LineCollider::AddLineSegment(vec2 p0, vec2 p1)
+{
+	ColliderLineSegment temp{};
+	temp.point[0] = p0;
+	temp.point[1] = p1;
+	m_lineSegments.push_back(temp);
+    lineCount++;
+}
+
+bool LineCollider::CheckIfColliding(const Collider* other)
+{
+    switch (other->getColliderType())
+    {
+        case ColliderType::ColliderTypeCircle:
+        {
+            const CircleCollider* circleCollider = dynamic_cast<const CircleCollider*>(other);
+
+            if (circleCollider)
+            {
+                Transform* lineTransform = Parent()->GetComponent<Transform>();
+                Transform* circleTransform = other->Parent()->GetComponent<Transform>();
+            }
+
+            return false;
+		}
+
+        case ColliderType::ColliderTypeLine:
+        {
+            return true; // Placeholder
+        }
+
+        default:
+        {
+            return false; // No collision detected
+        }
+    }
+    
+}
+
+/// @brief		  Read in the number of line segements to create.
+/// @param stream The json data to read from.
+void LineCollider::ReadNumLineSegments(Stream stream)
+{
+    m_NumSegments = stream.Read<int>();
+}
+
+/// @brief		  Read in the data for the line vectors.
+/// @param stream The json data to read from.
+void LineCollider::ReadLineVectors( Stream stream )
+{
+    // TODO: have to read in multiple vectors.
+    for ( auto& lineData : stream.getArray() )
+    {
+
+    }
+}
+
+std::map<std::string, ReadMethod<Component>> const& LineCollider::getReadMethods()
+{
+    return (std::map< std::string, ReadMethod< Component > > const&)readMethods;
+}
