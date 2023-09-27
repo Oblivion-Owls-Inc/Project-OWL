8348ff9a8f5c4e0b0d15d91f52cb5e2475cfd853
Tyler Birdsall <tyler.birdsall05@gmail.com>
2023-09-26 17:44:21 -0700

    fixed it

--- Source/AssetLibrarySystem.cpp
+++ Source/AssetLibrarySystem.cpp
@@ -1,8 +1,8 @@
 /// @file AssetLibrarySystem.cpp
 /// @author Tyler Birdsall (tyler.birdsall@digipen.edu)
-/// @brief AssetLibrary System 
+/// @brief AssetLibrarySystem
 /// @version 0.1
-/// @date 2023-09-26
+/// @date 2023-09-05
 /// 
 /// @copyright Copyright (c) 2023
 
@@ -12,23 +12,36 @@
 // virtual override methods
 //-----------------------------------------------------------------------------
 
-    
+
+//-----------------------------------------------------------------------------
+// private: reading
+//-----------------------------------------------------------------------------
+
+    /// @brief map of the AssetLibrarySystem read methods
+ReadMethodMap< AssetLibrarySystem > const AssetLibrarySystem::s_ReadMethods = {};
+
+/// @brief  gets this System's read methods
+/// @return this System's read methods
+ReadMethodMap< System > const& AssetLibrarySystem::GetReadMethods() const
+{
+    return (ReadMethodMap< System > const&)s_ReadMethods;
+}
 
 //-----------------------------------------------------------------------------
 // singleton stuff
 //-----------------------------------------------------------------------------
 
-/// @brief Constructs the AssetLibrarySystem
+    /// @brief Constructs the AssetLibrarySystem
 AssetLibrarySystem::AssetLibrarySystem() {}
 
 /// @brief The singleton instance of AssetLibrarySystem
-AssetLibrarySystem * AssetLibrarySystem::s_Instance = nullptr;
+AssetLibrarySystem* AssetLibrarySystem::s_Instance = nullptr;
 
 /// @brief gets the instance of AssetLibrarySystem
 /// @return the instance of the AssetLibrarySystem
-AssetLibrarySystem * AssetLibrarySystem::GetInstance()
+AssetLibrarySystem* AssetLibrarySystem::GetInstance()
 {
-    if ( s_Instance == nullptr )
+    if (s_Instance == nullptr)
     {
         s_Instance = new AssetLibrarySystem();
     }
