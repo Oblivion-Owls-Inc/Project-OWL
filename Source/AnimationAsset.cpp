64520f216ba5e694c922115edd6f7b7ae8689d19
Tybird0515 <Tybird0515@users.noreply.github.com>
2023-09-29 11:23:33 -0700

    AnimAsset doxygen

--- Source/AnimationAsset.cpp
+++ Source/AnimationAsset.cpp
@@ -12,6 +12,7 @@
 #include "Entity.h"
 #include "AnimationAsset.h"
 
+ /// @brief	Default constructor
 AnimationAsset::AnimationAsset()
 {
 	m_FrameStart = 0;
@@ -20,41 +21,57 @@ AnimationAsset::AnimationAsset()
 	m_Looping = false;
 }
 
+/// @brief	Gets the start frame of the animation
+/// @return	Start frame of the animation
 const unsigned AnimationAsset::GetStart() const
 {
 	return m_FrameStart;
 }
 
+/// @brief	Sets a new start frame
+/// @param	New start frame to set
 void AnimationAsset::SetStart(unsigned newStart)
 {
 	m_FrameStart = newStart;
 }
 
+/// @brief	Gets the end frame of the animation
+/// @return End frame of the animation
 const unsigned AnimationAsset::GetEnd() const
 {
 	return m_FrameEnd;
 }
 
+/// @brief	Sets a new end frame
+/// @param	New end frame to set
 void AnimationAsset::SetEnd(unsigned newEnd)
 {
 	m_FrameEnd = newEnd;
 }
 
+/// @brief	Gets the duration of the animation
+/// @return	Duration of the animation
 const float AnimationAsset::GetDuration() const
 {
 	return m_Duration;
 }
 
+/// @brief	Sets a new duration
+/// @param	New duration to set
 void AnimationAsset::SetDuration(float newDuration)
 {
 	m_Duration = newDuration;
 }
 
+/// @brief	Gets if the animation is looping
+/// @return	Looping status of the animation
 const bool AnimationAsset::GetLooping() const
 {
 	return	m_Looping;
 }
 
+/// @brief	Sets a new looping status
+/// @param	New looping status to set
 void AnimationAsset::SetLooping(bool newLooping)
 {
 	m_Looping = newLooping;
--- Source/AnimationAsset.h
+++ Source/AnimationAsset.h
@@ -16,18 +16,38 @@ class AnimationAsset
 {
 public:
 
+	/// @brief	Default constructor
 	AnimationAsset();
 
+	/// @brief	Gets the start frame of the animation
+	/// @return	Start frame of the animation
 	const unsigned GetStart() const;
+	/// @brief	Sets a new start frame
+	/// @param	New start frame to set
 	void SetStart(unsigned newStart);
 
+	/// @brief	Gets the end frame of the animation
+	/// @return End frame of the animation
 	const unsigned GetEnd() const;
+
+	/// @brief	Sets a new end frame
+	/// @param	New end frame to set
 	void SetEnd(unsigned newEnd);
 
+	/// @brief	Gets the duration of the animation
+	/// @return	Duration of the animation
 	const float GetDuration() const;
+
+	/// @brief	Sets a new duration
+	/// @param	New duration to set
 	void SetDuration(float newDuration);
 
+	/// @brief	Gets if the animation is looping
+	/// @return	Looping status of the animation
 	const bool GetLooping() const;
+
+	/// @brief	Sets a new looping status
+	/// @param	New looping status to set
 	void SetLooping(bool newLopping);
 
 private:
