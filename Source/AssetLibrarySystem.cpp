a1ff9d917270b16334875c374696f07a61a0b82d
Tybird0515 <Tybird0515@users.noreply.github.com>
2023-09-28 22:19:31 -0700

    commiting so I can grab main

--- Source/AssetLibrarySystem.cpp
+++ Source/AssetLibrarySystem.cpp
@@ -2,7 +2,7 @@
 /// @author Tyler Birdsall (tyler.birdsall@digipen.edu)
 /// @brief AssetLibrarySystem
 /// @version 0.1
-/// @date 2023-09-05
+/// @date 2023-09-26
 /// 
 /// @copyright Copyright (c) 2023
 
@@ -12,18 +12,88 @@
 // virtual override methods
 //-----------------------------------------------------------------------------
 
+template<class T>
+void AssetLibrarySystem<T>::OnSceneLoad()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		m_AssetList[i] = NULL;
+	}
+}
+
+template<class T>
+void AssetLibrarySystem<T>::OnSceneInit()
+{
+	/// 
+	/// read in from a json file what should be built
+	/// 
+}
+
+template<class T>
+void AssetLibrarySystem<T>::OnSceneExit()
+{
+	LibraryFlush();
+}
+
 //-----------------------------------------------------------------------------
 // public: public functions
 //-----------------------------------------------------------------------------
 
+/// @brief  Finds and returns an asset, builds if doesnt yet exist
+/// @return the constructed or found asset
+template<class T>
+T* AssetLibrarySystem<T>::LibraryGet()
+{
+	return nullptr;
+}
 
+/// @brief  Flushes everything in the library
+/// @brief  Automatically called on scene exit
+template<class T>
+void AssetLibrarySystem<T>::LibraryFlush()
+{
+}
+
+//-----------------------------------------------------------------------------
+// private: private functions
+//-----------------------------------------------------------------------------
+
+/// @brief  Adds an asset to the library
+/// @param	asset - the asset to add
+template<class T>
+void AssetLibrarySystem<T>::LibraryAdd(T* asset)
+{
+
+}
+
+/// @brief  Finds and returns an asset if it exists
+/// @return the found asset or nullptr is none is found
+template<class T>
+T* AssetLibrarySystem<T>::LibraryFind(const char* name)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		if (m_AssetList[i] != NULL)
+		{
+			if (strcmp(name, m_AssetList[i]->name))
+			{
+				return m_AssetList[i];
+			}
+		}
+	}
+	return nullptr;
+}
 
 //-----------------------------------------------------------------------------
 // private: reading
 //-----------------------------------------------------------------------------
 
-    /// @brief map of the AssetLibrarySystem read methods
-ReadMethodMap< AssetLibrarySystem > const AssetLibrarySystem::s_ReadMethods = {};
+/* commented out for now because it broke when I was templatizing lol, have fun
+
+/// @brief map of the AssetLibrarySystem read methods
+ReadMethodMap< AssetLibrarySystem const AssetLibrarySystem::s_ReadMethods = {};
+
+
 
 /// @brief  gets this System's read methods
 /// @return this System's read methods
@@ -36,7 +106,7 @@ ReadMethodMap< System > const& AssetLibrarySystem::GetReadMethods() const
 // singleton stuff
 //-----------------------------------------------------------------------------
 
-    /// @brief Constructs the AssetLibrarySystem
+/// @brief Constructs the AssetLibrarySystem
 AssetLibrarySystem::AssetLibrarySystem() {}
 
 /// @brief The singleton instance of AssetLibrarySystem
@@ -51,6 +121,6 @@ AssetLibrarySystem* AssetLibrarySystem::GetInstance()
         s_Instance = new AssetLibrarySystem();
     }
     return s_Instance;
-}
+}*/
 
 //-----------------------------------------------------------------------------
--- Source/AssetLibrarySystem.h
+++ Source/AssetLibrarySystem.h
@@ -2,7 +2,7 @@
 /// @author Tyler Birdsall (tyler.birdsall@digipen.edu)
 /// @brief Asset System
 /// @version 0.1
-/// @date 2023-09-05
+/// @date 2023-09-26
 /// 
 /// @copyright Copyright (c) 2023 Digipen Institute of Technology
 
@@ -11,6 +11,7 @@
 #include "System.h"
 
 /// @brief Asset System meant to be copy-pasted when creating new Systems
+template <class T>
 class AssetLibrarySystem : public System
 {
 
@@ -18,9 +19,57 @@ class AssetLibrarySystem : public System
 private: // virtual override methods
 //-----------------------------------------------------------------------------
 
+    /// @brief  Gets called once when this System is added to the Engine
+    virtual void OnInit() override {};
+
+    /// @brief  Gets called once every simulation frame. Use this function for anything that affects the simulation.
+    virtual void OnFixedUpdate() override {};
+
+    /// @brief  Gets called once every graphics frame. Do not use this function for anything that affects the simulation.
+    /// @param  dt  the elapsed time in seconds since the previous frame
+    virtual void OnUpdate(float dt) override {};
+
+    /// @brief  Gets called once before the Engine closes
+    virtual void OnExit() override {};
+
+    /// @brief  Gets called whenever a new Scene is loaded
+    virtual void OnSceneLoad() override;
+
+    /// @brief  Gets called whenever a scene is initialized
+    virtual void OnSceneInit() override;
+
+    /// @brief  Gets called whenever a scene is exited
+    virtual void OnSceneExit() override;
+
 //-----------------------------------------------------------------------------
 public: // public functions
 //-----------------------------------------------------------------------------
+
+    /// @brief  Finds and returns an asset, builds if doesnt yet exist
+    /// @return the constructed or found asset
+    T* LibraryGet();
+
+    /// @brief  Flushes everything in the library
+    /// @brief  Automatically called on scene exit
+    void LibraryFlush();
+
+//-----------------------------------------------------------------------------
+private: // private variables
+//-----------------------------------------------------------------------------
+
+    // 100 is arbitrary, tell me if you need more/it to dynamically grow
+    T* m_AssetList[100];
+
+//-----------------------------------------------------------------------------
+private: // private functions
+//-----------------------------------------------------------------------------
+
+    /// @brief  Adds an asset to the library
+    void LibraryAdd(T* asset);
+
+    /// @brief  Finds and returns an asset if it exists
+    /// @return the found asset or nullptr is none is found
+    T* LibraryFind(const char* name);
 
 //-----------------------------------------------------------------------------
 private: // reading
