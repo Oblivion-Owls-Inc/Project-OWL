d3eb060c780d27b2a777ec25d6e92194a31ab7da
Steve Bukowinski <steve.bukowinski@digipen.edu>
2023-10-11 12:34:19 -0700

    Add error messages when ComponentFactory fails

--- Source/ComponentFactory.cpp
+++ Source/ComponentFactory.cpp
@@ -28,39 +28,84 @@
 #include "Tilemap.h"
 
 
-/// @brief     Creates a component of a specified type.
-/// @param key The type of component being created.
-/// @return    A pointer to the new component.
-Component* ComponentFactory::Create( std::string const& key )
-{
-    return s_ComponentTypes.at( key ).second();
-}
+//-----------------------------------------------------------------------------
+// public: methods
+//-----------------------------------------------------------------------------
 
-std::type_index ComponentFactory::GetTypeId( std::string const& typeName )
-{
-    return s_ComponentTypes.at( typeName ).first;
-}
+    /// @brief  creates a new Component with the specified type
+    /// @param  typeName    the type name of Component to create
+    /// @return the created component
+    Component* ComponentFactory::Create( std::string const& typeName )
+    {
+        return GetComponentInfo( typeName ).second();
+    }
 
-template < typename ComponentType >
-Component* ComponentFactory::Creator()
-{
-    return new ComponentType();
-}
+    /// @brief  gets the std::type_index of the Component type with the specified name
+    /// @param  typeName    the type name of the Component type to get
+    /// @return the std::type_index of the Component type
+    std::type_index ComponentFactory::GetTypeId( std::string const& typeName )
+    {
+        return GetComponentInfo( typeName ).first;
+    }
 
-// Fill the map with constructors from the different components.
-std::map< std::string, std::pair< std::type_index, Component* (*)() > > const ComponentFactory::s_ComponentTypes = {
-    { "Transform"        , ComponentInfo<Transform>()        },
-    { "Sprite"           , ComponentInfo<Sprite>()           },
-    { "RigidBody"        , ComponentInfo<RigidBody>()        },
-    { "MovementAI"       , ComponentInfo<MovementAI>()       },
-    { "LineCollider"     , ComponentInfo<LineCollider>()     },
-    { "CircleCollider"   , ComponentInfo<CircleCollider>()   },
-    { "AudioPlayer"      , ComponentInfo<AudioPlayer>()      },
-    { "Animation"        , ComponentInfo<Animation>()        },
-    { "Text"             , ComponentInfo<Text>()             },
-    { "PlayerController" , ComponentInfo<PlayerController>() },
-    { "TurretBehavior"   , ComponentInfo<TurretBehavior>()   },
-    { "EffectAnimator"   , ComponentInfo<EffectAnimator>()   },
-    { "Tilemap"          , ComponentInfo<Tilemap>()          },
-    { "TilemapSprite"    , ComponentInfo<TilemapSprite>()    }
-};
+//-----------------------------------------------------------------------------
+// private: methods
+//-----------------------------------------------------------------------------
+
+    /// @brief  accesses the Component type map with error handling
+    /// @param  typeName    the type of component to get info about
+    /// @return the Component type info for the specified type name
+    std::pair< std::type_index, Component* (*)() > const& ComponentFactory::GetComponentInfo( std::string const& typeName )
+    {
+        auto iterator = s_ComponentTypes.find( typeName );
+        if ( iterator == s_ComponentTypes.end() )
+        {
+            throw std::runtime_error(
+                std::string() +
+                "Error: could not find Component Type with name \"" +
+                typeName + '\"'
+            );
+        }
+        return iterator->second;
+    }
+
+//-----------------------------------------------------------------------------
+// private: data
+//-----------------------------------------------------------------------------
+
+    /// @brief  template method to create the specified type of Component
+    /// @tparam ComponentType   the type of Component to create
+    /// @return the created Component
+    template < typename ComponentType >
+    Component* ComponentFactory::Creator()
+    {
+        return new ComponentType();
+    }
+
+    /// @brief  template method for creating Type information
+    /// @tparam ComponentType   the type of Component to create information for
+    /// @return the Component information
+    template < typename ComponentType >
+    std::pair< std::type_index, Component* (*)() > ComponentFactory::ComponentInfo() {
+        return { typeid( ComponentType ), Creator< ComponentType > };
+    }
+
+    /// @brief  map containing information on Component types by name
+    std::map< std::string, std::pair< std::type_index, Component* (*)() > > const ComponentFactory::s_ComponentTypes = {
+        { "Transform"        , ComponentInfo<Transform>()        },
+        { "Sprite"           , ComponentInfo<Sprite>()           },
+        { "RigidBody"        , ComponentInfo<RigidBody>()        },
+        { "MovementAI"       , ComponentInfo<MovementAI>()       },
+        { "LineCollider"     , ComponentInfo<LineCollider>()     },
+        { "CircleCollider"   , ComponentInfo<CircleCollider>()   },
+        { "AudioPlayer"      , ComponentInfo<AudioPlayer>()      },
+        { "Animation"        , ComponentInfo<Animation>()        },
+        { "Text"             , ComponentInfo<Text>()             },
+        { "PlayerController" , ComponentInfo<PlayerController>() },
+        { "TurretBehavior"   , ComponentInfo<TurretBehavior>()   },
+        { "EffectAnimator"   , ComponentInfo<EffectAnimator>()   },
+        { "Tilemap"          , ComponentInfo<Tilemap>()          },
+        { "TilemapSprite"    , ComponentInfo<TilemapSprite>()    }
+    };
+
+//-----------------------------------------------------------------------------
