b20e4589fc2aa044a27c1cde2ea88461885435c5
aidanstraker <aidanstraker@users.noreply.github.com>
2023-10-14 13:41:33 -0700

    Audioplayer

--- Source/AudioPlayer.cpp
+++ Source/AudioPlayer.cpp
@@ -129,35 +129,35 @@
     /// @param  data    the json data
     void AudioPlayer::readSound( nlohmann::json const& data )
     {
-        m_Sound = AssetLibrary<Sound>()->GetAsset( data.Read<std::string>() );
+        m_Sound = AssetLibrary<Sound>()->GetAsset( Stream::Read<std::string>(data) );
     }
 
     /// @brief  read the volume of this component from json
     /// @param  data    the json data
     void AudioPlayer::readVolume( nlohmann::json const& data )
     {
-        m_Volume = data.Read<float>();
+        m_Volume = Stream::Read<float>(data);
     }
 
     /// @brief  read the pitch of this component from json
     /// @param  data    the json data
     void AudioPlayer::readPitch( nlohmann::json const& data )
     {
-        m_Pitch = data.Read<float>();
+        m_Pitch = Stream::Read<float>(data);
     }
 
     /// @brief  read the volumeVariance of this component from json
     /// @param  data    the json data
     void AudioPlayer::readVolumeVariance( nlohmann::json const& data )
     {
-        m_VolumeVariance = data.Read<float>();
+        m_VolumeVariance = Stream::Read<float>(data);
     }
 
     /// @brief  read the pitchVariance of this component from json
     /// @param  data    the json data
     void AudioPlayer::readPitchVariance( nlohmann::json const& data )
     {
-        m_PitchVariance = data.Read<float>();
+        m_PitchVariance = Stream::Read<float>(data);
     }
 
     /// @brief  map of the read methods for this Component
