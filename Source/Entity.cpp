69d085d36d41ac0ca46e735e0e88bcacb77770b6
SixSleepyAsh <76552849+SixSleepyAsh@users.noreply.github.com>
2023-11-03 13:22:11 -0700

    Added New Entity Button and Added Delete Modal (#39)

--- Source/Entity.cpp
+++ Source/Entity.cpp
@@ -13,6 +13,7 @@
 #include "Component.h"		  // Type
 #include <cassert>			  // assert
 #include "ComponentFactory.h" // Create.
+#include "InputSystem.h"
 #include "AssetLibrarySystem.h"
 #include "DebugSystem.h"
 #include "basics.h"
@@ -124,9 +125,18 @@
         }
 
         static char name[ 128 ];
-        strcpy_s( name, 128, m_Name.c_str() );
-        ImGui::InputText( "Entity Name", name, 128 );
-        m_Name = name;
+        //strcpy_s( name, 128, m_Name.c_str() )
+        ImGui::InputText("Entity Name", name, 128);
+
+        if (ImGui::Button("Rename Entity") || 
+            Input()->GetKeyTriggered(GLFW_KEY_ENTER))
+        {
+            if (strlen(name) > 0)
+            {
+                Debug() << "Renamed Entity " << m_Name << " to " << name << "\n";
+                m_Name = name;
+            }
+        }
 
         for (const auto& componentPair : this->getComponents())
         {
