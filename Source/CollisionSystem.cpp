e107ab217b39313f62eb091bf1a033290ac961dc
unknown <digipen@dit2859us.DIGIPEN.EDU>
2024-01-12 17:29:43 -0800

    added Ty's most recent level

--- Source/CollisionSystem.cpp
+++ Source/CollisionSystem.cpp
@@ -49,9 +49,11 @@
     /// @param  collider    the collider to remove
     void CollisionSystem::removeCollider( Collider* collider )
     {
-        m_Colliders.erase(
-            std::remove( m_Colliders.begin(), m_Colliders.end(), collider )
-        );
+        auto it = std::remove( m_Colliders.begin(), m_Colliders.end(), collider );
+        if ( it != m_Colliders.end() )
+        {
+            m_Colliders.erase( it );
+        }
     }
 
 
