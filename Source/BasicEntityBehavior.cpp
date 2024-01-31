e984b6bae49c3e7eb69b01fcd6e485a42e2eea3f
Aidan Straker <42103315+suncrest45@users.noreply.github.com>
2024-01-31 14:47:29 -0800

    Fixed issue

--- Source/BasicEntityBehavior.cpp
+++ Source/BasicEntityBehavior.cpp
@@ -51,7 +51,7 @@ void BasicEntityBehavior::OnExit()
 /// @brief inspector for this component
 void BasicEntityBehavior::Inspector()
 {
-    m_Health.Inspect();
+    m_Health.Inspector();
 }
 
 ///-----------------------------------------------------------------------------
