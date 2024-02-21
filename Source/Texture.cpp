26f5d1b42bbcc084afd843e3fece77cc9465b208
Steve Bukowinski <steve.bukowinski@digipen.edu>
2024-02-20 17:22:34 -0800

    Make UiButtons detect the topmost hovered sprite

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -93,6 +93,11 @@
             reloadMesh();
         }
 
+        if ( ImGui::Checkbox( "Use Aspect Ratio", &m_UseAspectRatio ) )
+        {
+            reloadMesh();
+        }
+
         if ( ImGui::Button( "Reload Texture" ) )
         {
             loadImage();
@@ -286,7 +291,7 @@ void Texture::loadImage()
         }
         else
         {
-            m_Mesh = new Mesh( glm::vec2( GetAspectRatio(), 1 ), m_SheetDimensions, m_Pivot );
+            m_Mesh = new Mesh( m_UseAspectRatio ? glm::vec2( GetAspectRatio(), 1.0f ) : glm::vec2( 1.0f ), m_SheetDimensions, m_Pivot );
         }
     }
 
