8d32f74221a0f21549c4f5259857e4b0bda33853
stevembuko <stevembuko@users.noreply.github.com>
2024-03-01 21:05:53 +0000

    Update documentation

--- doc_output/html/search/classes_b.js
+++ doc_output/html/search/classes_b.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['particle_1015',['Particle',['../structParticleSystem_1_1Particle.html',1,'ParticleSystem']]],
-  ['particlesystem_1016',['ParticleSystem',['../classParticleSystem.html',1,'']]],
-  ['pathfinder_1017',['Pathfinder',['../classPathfinder.html',1,'']]],
-  ['pausesystem_1018',['PauseSystem',['../classPauseSystem.html',1,'']]],
-  ['platformsystem_1019',['PlatformSystem',['../classPlatformSystem.html',1,'']]],
-  ['playbar_1020',['PlayBar',['../classPlayBar.html',1,'']]],
-  ['playercontroller_1021',['PlayerController',['../classPlayerController.html',1,'']]],
-  ['pool_1022',['Pool',['../classPool.html',1,'']]],
-  ['pool_3c_20float_20_3e_1023',['Pool&lt; float &gt;',['../classPool.html',1,'']]],
-  ['pool_3c_20int_20_3e_1024',['Pool&lt; int &gt;',['../classPool.html',1,'']]]
+  ['particle_1018',['Particle',['../structParticleSystem_1_1Particle.html',1,'ParticleSystem']]],
+  ['particlesystem_1019',['ParticleSystem',['../classParticleSystem.html',1,'']]],
+  ['pathfinder_1020',['Pathfinder',['../classPathfinder.html',1,'']]],
+  ['pausesystem_1021',['PauseSystem',['../classPauseSystem.html',1,'']]],
+  ['platformsystem_1022',['PlatformSystem',['../classPlatformSystem.html',1,'']]],
+  ['playbar_1023',['PlayBar',['../classPlayBar.html',1,'']]],
+  ['playercontroller_1024',['PlayerController',['../classPlayerController.html',1,'']]],
+  ['pool_1025',['Pool',['../classPool.html',1,'']]],
+  ['pool_3c_20float_20_3e_1026',['Pool&lt; float &gt;',['../classPool.html',1,'']]],
+  ['pool_3c_20int_20_3e_1027',['Pool&lt; int &gt;',['../classPool.html',1,'']]]
 ];
