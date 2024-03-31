f1e3b5d98101a8f0a0702211ac0aed0eafe1de67
Steve Bukowinski <steve.bukowinski@digipen.edu>
2024-03-31 11:37:18 -0700

    Interactable Component, WIP Interactor Component

---
+++ Source/Interactable.cpp
@@ -0,0 +1,258 @@
+/// @file       Interactable.cpp
+/// @author     Steve Bukowinski (steve.bukowinski@digipen.edu)
+/// @brief      Component which can be interacted with by pressing a button when nearby
+/// @version    0.1
+/// @date       March 2024
+/// 
+/// @copyright  Copyright (c) 2024 Digipen Institute of Technology
+
+#pragma once
+
+#include "Interactable.h"
+
+#include "ComponentSystem.h"
+
+#include "ComponentReference.t.h"
+#include "Transform.h"
+#include "Sprite.h"
+
+
+//-----------------------------------------------------------------------------
+// public: constructor / Destructor
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  default constructor
+    Interactable::Interactable() :
+        Component( typeid( Interactable ) )
+    {}
+
+
+//-----------------------------------------------------------------------------
+// public: methods
+//-----------------------------------------------------------------------------
+
+    
+    /// @brief  interacts with this Interactable
+    void Interactable::Interact()
+    {
+        for ( auto& [ ownerId, callback ] : m_OnInteractCallbacks )
+        {
+            callback();
+        }
+    }
+
+
+    /// @brief  adds a callback to be called when this Interactable Component is interacted with
+    /// @param  ownerId     the ID of the owner of the callback
+    /// @param  callback    the callback to add
+    void Interactable::AddOnInteractCallback( unsigned ownerId, std::function< void() > callback )
+    {
+        m_OnInteractCallbacks.emplace_back( ownerId, callback );
+    }
+
+    /// @brief  removes an OnInteractCallback from this Interactable Component
+    /// @param  ownerId     the ownerId of the callback to remove
+    void Interactable::RemoveOnInteractCallback( unsigned ownerId )
+    {
+        auto it = std::find_if(
+            m_OnInteractCallbacks.begin(), m_OnInteractCallbacks.end(),
+            [ ownerId ]( std::pair< unsigned, std::function< void () > > const& callbackPair ) -> bool
+            {
+                return callbackPair.first == ownerId;
+            }
+        );
+        if ( it == m_OnInteractCallbacks.end() )
+        {
+            Debug() << "ERROR: could not find OnInteractCallbacks with ownerId " << ownerId << " to remove (" << GetName << ")" << std::endl;
+            return;
+        }
+
+        m_OnInteractCallbacks.erase( it );
+    }
+
+
+//-----------------------------------------------------------------------------
+// public: accessors
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  gets the radius at which this Interactable can be interacted with
+    /// @return the radius at which this Interactable can be interacted with
+    float Interactable::GetInteractionRadius() const
+    {
+        return m_InteractionRadius;
+    }
+
+    /// @brief  sets the radius at which this Interactable can be interacted with
+    /// @param  radius  the radius at which this Interactable can be interacted with
+    void Interactable::SetInteractionRadius( float radius )
+    {
+        m_InteractionRadius = radius;
+    }
+
+
+    /// @brief  gets the Transform attached to this Interactable
+    /// @return the Transform attached to this Interactable
+    Transform const* Interactable::GetTransform() const
+    {
+        return m_Transform;
+    }
+
+    /// @brief  gets the Sprite used to display the interact control prompt
+    /// @return the Sprite used to display the interact control prompt
+    Sprite* Interactable::GetPromptSprite()
+    {
+        return m_PromptSprite;
+    }
+
+    /// @brief  gets the Interact Action used to interact with this Interactable
+    /// @return the Interact Action used to interact with this Interactable
+    Action const* Interactable::GetInteractAction() const
+    {
+        return m_InteractAction;
+    }
+
+
+//-----------------------------------------------------------------------------
+// public: virtual override methods
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  called once when entering the scene
+    void Interactable::OnInit()
+    {
+        Components< Interactable >()->AddComponent( this );
+
+        m_Transform.Init( GetEntity() );
+
+        m_InteractAction.Init();
+
+        m_PromptEntity.SetOwnerName( GetName() );
+        m_PromptEntity.Init();
+    }
+
+
+    /// @brief  called once when exiting the scene
+    void Interactable::OnExit()
+    {
+        Components< Interactable >()->RemoveComponent( this );
+
+        m_Transform.Exit();
+
+        m_InteractAction.Exit();
+
+        m_PromptEntity.Exit();
+    }
+
+
+//-----------------------------------------------------------------------------
+// private: methods
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// public: inspection
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  shows the inspector for Interactable
+    void Interactable::Inspector()
+    {
+        ImGui::DragFloat( "Interaction Radius", &m_InteractionRadius, 0.05f, 0.0f, INFINITY );
+
+        m_InteractAction.Inspect( "Interact Control Action" );
+
+        m_PromptEntity.Inspect( "Prompt Entity" );
+    }
+
+
+//-----------------------------------------------------------------------------
+// private: reading
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  reads the radius at which this Interactable can be interacted with
+    /// @param  data    the JSON data to read from
+    void Interactable::readInteractionRadius( nlohmann::ordered_json const& data )
+    {
+        Stream::Read( m_InteractionRadius, data );
+    }
+
+    /// @brief  reads the control Action used to interact with this Interactable
+    /// @param  data    the JSON data to read from
+    void Interactable::readInteractAction( nlohmann::ordered_json const& data )
+    {
+        Stream::Read( m_InteractAction, data );
+    }
+
+    /// @brief  reads the Entity used to display the interact control prompt
+    /// @param  data    the JSON data to read from
+    void Interactable::readPromptEntity( nlohmann::ordered_json const& data )
+    {
+        Stream::Read( m_PromptEntity, data );
+    }
+
+
+//-----------------------------------------------------------------------------
+// public: reading / writing
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  gets the map of read methods for this Interactable
+    /// @return the map of read methods for this Interactable
+    ReadMethodMap< ISerializable > const& Interactable::GetReadMethods() const
+    {
+        static ReadMethodMap< Interactable > const readMethods = {
+            { "InteractionRadius", &Interactable::readInteractionRadius },
+            { "InteractAction"   , &Interactable::readInteractAction    },
+            { "PromptEntity"     , &Interactable::readPromptEntity      }
+        };
+
+        return (ReadMethodMap< ISerializable > const&)readMethods;
+    }
+
+
+    /// @brief  writes this Interactable to JSON
+    /// @return the JSON data of this Interactable
+    nlohmann::ordered_json Interactable::Write() const
+    {
+        nlohmann::ordered_json json = nlohmann::ordered_json::object();
+
+        json[ "InteractionRadius" ] = Stream::Write( m_InteractionRadius );
+        json[ "InteractAction"    ] = Stream::Write( m_InteractAction    );
+        json[ "PromptEntity"      ] = Stream::Write( m_PromptEntity      );
+
+        return json;
+    }
+
+    
+//-----------------------------------------------------------------------------
+// public: copying
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  clones this Interactable
+    /// @return the newly created clone of this Interactable
+    Interactable* Interactable::Clone() const
+    {
+        return new Interactable( *this );
+    }
+
+
+//-----------------------------------------------------------------------------
+// private: copying
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  copy-constructor for the Interactable
+    /// @param  other   the other Interactable to copy
+    Interactable::Interactable( Interactable const& other ) :
+        Component( other ),
+        m_InteractionRadius( other.m_InteractionRadius ),
+        m_InteractAction   ( other.m_InteractAction    ),
+        m_PromptEntity     ( other.m_PromptEntity, { &m_PromptSprite } )
+    {}
+
+
+//-----------------------------------------------------------------------------
---
+++ Source/Interactor.h
@@ -0,0 +1,127 @@
+/// @file       Interactor.h
+/// @author     Steve Bukowinski (steve.bukowinski@digipen.edu)
+/// @brief      Behavior that can interact with nearby Interactable Components
+/// @version    0.1
+/// @date       March 2024
+/// 
+/// @copyright  Copyright (c) 2024 Digipen Institute of Technology
+
+#pragma once
+
+#include "Behavior.h"
+
+#include "ComponentReference.h"
+class Transform;
+class Interactable;
+
+
+/// @brief  Behavior that can interact with nearby Interactable Components
+class Interactor : public Behavior
+{
+//-----------------------------------------------------------------------------
+public: // constructor / Destructor
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  default constructor
+    Interactor();
+
+
+//-----------------------------------------------------------------------------
+public: // methods
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+public: // accessors
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+public: // virtual override methods
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  called once when entering the scene
+    virtual void OnInit() override;
+
+    /// @brief  called once when exiting the scene
+    virtual void OnExit() override;
+
+
+//-----------------------------------------------------------------------------
+private: // members
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  the Transform Component attached to this Interactor
+    ComponentReference< Transform > m_Transform;
+
+    /// @brief  the Interactable Component currently being targeted by this Interactor
+    Interactable* m_TargetedInteractable;
+
+
+//-----------------------------------------------------------------------------
+private: // methods
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  updates which Interactable Component is currently being targeted
+    void updateTargetedInteractable();
+
+
+//-----------------------------------------------------------------------------
+public: // inspection
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  shows the inspector for Interactor
+    virtual void Inspector() override;
+
+
+//-----------------------------------------------------------------------------
+private: // reading
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+public: // reading / writing
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  gets the map of read methods for this Interactor
+    /// @return the map of read methods for this Interactor
+    virtual ReadMethodMap< ISerializable > const& GetReadMethods() const override;
+
+
+    /// @brief  writes this Interactor to JSON
+    /// @return the JSON data of this Interactor
+    virtual nlohmann::ordered_json Write() const override;
+
+    
+//-----------------------------------------------------------------------------
+public: // copying
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  clones this Interactor
+    /// @return the newly created clone of this Interactor
+    virtual Interactor* Clone() const override;
+
+
+//-----------------------------------------------------------------------------
+private: // copying
+//-----------------------------------------------------------------------------
+
+
+    /// @brief  copy-constructor for the Interactor
+    /// @param  other   the other Interactor to copy
+    Interactor( Interactor const& other );
+
+
+    // disable assignment operator
+    void operator =( Interactor const& ) = delete;
+
+
+//-----------------------------------------------------------------------------
+};
