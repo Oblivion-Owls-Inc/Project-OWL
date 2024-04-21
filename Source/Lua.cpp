0b6de099f5fda6a8afbc13e8f5caf4741b73d29c
Tyler Birdsall <tyler.birdsall05@gmail.com>
2024-04-20 20:21:35 -0700

    Lua

--- Source/Lua.cpp
+++ Source/Lua.cpp
@@ -1,3 +1,10 @@
+/// \file     Lua.cpp
+/// \author   Jax Clayton (jax.clayton@digipen.edu)
+/// 
+/// \brief    A version of Sprite specifically for rendering tiles.
+/// 
+/// \copyright Copyright (c) 2024 Digipen Institute of Technology
+
 #include "pch.h" // precompiled header has to be included first
 
 
