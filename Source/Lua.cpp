07b54fb7863d2762f66ed0486a59c7359031dba9
SixSleepyAsh <Claytonjax88@gmail.com>
2024-04-20 20:47:22 -0700

    lua/main

--- Source/Lua.cpp
+++ Source/Lua.cpp
@@ -1,9 +1,11 @@
-/// \file     Lua.cpp
-/// \author   Jax Clayton (jax.clayton@digipen.edu)
+////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file     Main.cpp
+/// @author   Jax Clayton (jax.clayton@digipen.edu)
 /// 
-/// \brief    A version of Sprite specifically for rendering tiles.
+/// @brief    Main entry point for the game for the Lua scripting language.
 /// 
-/// \copyright Copyright (c) 2024 Digipen Institute of Technology
+/// @copyright Copyright (c) 2024 Digipen Institute of Technology
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "pch.h" // precompiled header has to be included first
 
